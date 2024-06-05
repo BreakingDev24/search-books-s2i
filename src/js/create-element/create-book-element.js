ec82fd4f0ff9759b37bf31c839293f07f8bd612d
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-05 16:51:04 +0200

    add key variable and create button

--- src/js/create-element/create-book-element.js
+++ src/js/create-element/create-book-element.js
@@ -5,13 +5,17 @@ export default function createBookElement(booksDetails){
     const library = document.getElementById('library')
 
     booksDetails.map(detail => {
-        const {title, authors} = detail
+        const {title, authors, key} = detail
         const bookDiv = createElement('div', 'book-div', library)
+        bookDiv.dataset.key = key
 
         const bookTitle = createElement('h2', 'book-title', bookDiv)
         bookTitle.textContent = title
 
         const bookAuthors = createElement('p', 'book-authors', bookDiv)
         bookAuthors.textContent = authors
+
+        const descriptionBtn = createElement('button', 'description-btn', bookDiv)
+        descriptionBtn.textContent = 'about'
     })
 }
