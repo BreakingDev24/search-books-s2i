ed4dc2d18006d42cb28e34f637283e85b3b1701b
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-06 15:09:09 +0200

    "Improve description retrieval logic to handle both string and object types"

--- src/js/api/fetch-description.js
+++ src/js/api/fetch-description.js
@@ -4,9 +4,10 @@ export const fetchDescription = async (key) => {
     try{
         const descriptionUrl = `https://openlibrary.org${key}.json`
         const response = await axios.get(descriptionUrl);
-        const bookDescription = getDescription(response)
+        const bookDescription = getDescription(response);
+        console.log(response);
         return bookDescription
-        console.log(bookDescription);
+        
     } catch(err){
         console.log(err);
     }
@@ -15,6 +16,6 @@ export const fetchDescription = async (key) => {
 
 const getDescription = (key) => {
     const data =  _.get(key, 'data', {});
-    const description = _.get(data, 'description', '');
+    const description = _.isObject(data.description) ? _.get(data, 'description.value', 'No description found') : _.get(data, 'description', 'No description found');
     return description
 }
