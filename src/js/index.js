bc7b66cd9ca20e7e042d2314d335d99e62dbe9da
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-10 23:42:12 +0200

    added imports and event listener for view button toggle and viewport check

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,14 @@ import '../style/styles.scss'
 
 import { fetchBookData } from "./api/fetch-book";
 import { getInput } from "./display-element/displayBook";
-
+import { changeView } from './components/viewButtonToggle';
+import { handleViewportCheck } from './components/viewportChecker';
 
 const searchBtn = document.getElementById('search-btn');
+const viewBtnContainer = document.getElementById('view-btn-container')
 
-searchBtn.addEventListener('click', getInput)
+console.log(viewBtnContainer);
+// searchBtn.addEventListener('click', getInput)
+fetchBookData('love')
+handleViewportCheck()
+viewBtnContainer.addEventListener('click', changeView)
