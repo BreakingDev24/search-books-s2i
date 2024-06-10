1c513f34b2473370cd2efc2d41349134889002fe
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-10 21:19:35 +0200

    placed title and authors inside info container

--- src/js/create-element/create-book-element.js
+++ src/js/create-element/create-book-element.js
@@ -11,10 +11,12 @@ export default function createBookElement(booksDetails){
         const bookDiv = createElement('div', 'book-div', library)
         bookDiv.dataset.key = key
 
-        const bookTitle = createElement('h2', 'book-title', bookDiv)
+        const bookInfoContainer = createElement('div', 'info-container', bookDiv)
+
+        const bookTitle = createElement('h2', 'book-title', bookInfoContainer)
         bookTitle.textContent = title
 
-        const bookAuthors = createElement('p', 'book-authors', bookDiv)
+        const bookAuthors = createElement('p', 'book-authors', bookInfoContainer)
         bookAuthors.textContent = authors
 
         const descriptionBtn = createElement('button', 'description-btn', bookDiv)
