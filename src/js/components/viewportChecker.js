e4ef51edad2b55b7936b35714ee68ed6b1fe3627
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-11 16:33:32 +0200

    import setGridView lastClickedButton, library

--- src/js/components/viewportChecker.js
+++ src/js/components/viewportChecker.js
@@ -1,10 +1,15 @@
-import { setListView } from "./viewButtonToggle";
+import { setListView, setGridView, lastClickedButton, library } from "./viewButtonToggle";
 
 function checkViewport() {
-    if (window.innerWidth <= 600) {
+    console.log(lastClickedButton);
+    if (window.innerWidth <= 600 || lastClickedButton.id === 'list-btn') {
         setListView();
+    } else {
+        setGridView()
     }
 }
+
+
 
 export function handleViewportCheck(){
     checkViewport();
