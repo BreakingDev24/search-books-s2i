4d523cde290dbb899477fc4617b5bba64a5b4223
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-11 17:53:36 +0200

    change winow inner width size and import set active button on load

--- src/js/components/viewportChecker.js
+++ src/js/components/viewportChecker.js
@@ -1,19 +1,21 @@
-import { setListView, setGridView, lastClickedButton, library } from "./viewButtonToggle";
+import { setListView, setGridView,setActviteButtonOnLoad, lastClickedButton, library } from "./viewButtonToggle";
 
 function checkViewport() {
     console.log(lastClickedButton);
-    if (window.innerWidth <= 600 || lastClickedButton.id === 'list-btn') {
+    if (window.innerWidth <= 428 || lastClickedButton.id === 'list-btn') {
         setListView();
     } else {
         setGridView()
+        setActviteButtonOnLoad()
     }
 }
 
 
 
+
+
 export function handleViewportCheck(){
     checkViewport();
-    
     window.addEventListener('resize', checkViewport);
 
 }
