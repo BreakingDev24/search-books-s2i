7587e44dfb837499695661cabeae22fdec0fc7d1
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-19 14:46:33 +0200

    Updated addBookToWishList function and integrated renderMylibrary

--- src/js/components/wishlist/addToWishlist.js
+++ src/js/components/wishlist/addToWishlist.js
@@ -1,16 +1,19 @@
+import { renderMylibrary } from "./renderMyLibrary";
+
 const myLibrary = []
 
 export function addBookToWishList(book, btn){
     if(!myLibrary.includes(book)){
         myLibrary.push(book);
         btn.innerHTML = '<i class="bi bi-file-minus"></i>';
+        renderMylibrary(myLibrary)
         console.log(myLibrary);
     } else {
         myLibrary.splice(myLibrary.indexOf(book), 1)
-        btn.innerHTML = '<i class="bi bi-plus-circle"></i>'
+        btn.innerHTML = '<i class="bi bi-plus-circle"></i>';
+        renderMylibrary(myLibrary)
         console.log(myLibrary)
     }
-
     updateLibraryCounter(myLibrary.length)
 }
 
