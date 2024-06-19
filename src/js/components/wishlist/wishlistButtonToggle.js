0f17a79c98e85f041c8b51679bc71d26f68f35bc
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-19 16:30:04 +0200

    add overlay

--- src/js/components/wishlist/wishlistButtonToggle.js
+++ src/js/components/wishlist/wishlistButtonToggle.js
@@ -2,10 +2,14 @@
 
 export function wishlsitToggle(){
     const sideContainer = document.getElementById('side-container');
+    const overlay = document.getElementById('overlay');
     if(sideContainer.classList.contains('show')){
-        sideContainer.classList.remove('show')
+        sideContainer.classList.remove('show');
+        overlay.classList.remove('show');
     } else {
-        sideContainer.classList.add('show')
+        sideContainer.classList.add('show');
+        overlay.classList.add('show')
     }
 
 }
+
